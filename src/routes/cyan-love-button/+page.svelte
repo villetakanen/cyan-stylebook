963fd82625008c8847ce7fbbd088e46f49d72a0e
Ville Takanen <ville.takanen@futurice.com>
2022-11-23 12:42:37 +0200

    feat: love-button story

--- src/routes/cyan-love-button/+page.svelte
+++ src/routes/cyan-love-button/+page.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
 import AppBar from "$lib/AppBar.svelte";
+import type { CyanLoveButton } from "src/cyan-elements/src";
 
 let count = 4
 function handleClick(e: CustomEvent) {
-  count = e.detail
+  count = (e.target as CyanLoveButton).count
 }
 </script>
-
-
 
 <AppBar />
 
 <main class="bookLayout">
   <article class="Column">
     <h1>Cyan-love-button</h1>
-    <p>Here is a cyan love button.</p>
-    <cyan-love-button count={count} on:change={handleClick} />
+    <p>Here is a cyan love button with {count} likes.</p>
+    <cyan-toolbar small>
+      <cyan-spacer></cyan-spacer>
+      <cyan-love-button count={count} on:change={handleClick} />
+    </cyan-toolbar>
   </article>
 </main>
