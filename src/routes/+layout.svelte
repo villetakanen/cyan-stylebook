f9859a4eb0530e48c65a2e47ee1918a8a38d41d2
Ville Takanen <ville.takanen@futurice.com>
2022-11-24 09:48:08 +0200

    fix: added top-app-bar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<cyan-top-app-bar title="Cyan Elements Stylebook" menu>
+  <cyan-lightmode-toggle />
+</cyan-top-app-bar>
+<nav id="tray">
+  <a href="/">
+    <cyan-nav-button text noun="stylebook">Introduction</cyan-nav-button>
+  </a>
+  <hr>
+  <a href="/cyan-top-app-bar">
+    <cyan-nav-button noun="page">cyan-top-app-bar</cyan-nav-button>
+  </a>
+</nav>
+
+<slot></slot>
+
+<style lang="sass">
+#tray
+  position: fixed
+  top: 72px
+  left: 0
+  display: flex
+  flex-direction: column
+  gap: 8px
+  width: 220px
+  padding-left: 16px
+</style>
---
+++ src/routes/cyan-top-app-bar/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+
+let menu = false;
+let sticky = false;
+let modal = false;
+
+const toggleMenu = () => {
+  menu = !menu;
+};
+const toggleModal = () => {
+  modal = !modal;
+};
+const toggleSticky = () => {
+  sticky = !sticky;
+};
+</script>
+
+<main class="bookLayout">
+  <article class="Column full">
+    <cyan-top-app-bar menu={menu} sticky={sticky} modal={modal}>
+      <cyan-button text noun="kebab" on:click={toggleMenu} on:keydown={toggleMenu} active={menu}/>
+      <cyan-button text noun="check" on:click={toggleSticky} on:keydown={toggleSticky} active={modal}/>
+      <cyan-button text noun="back" on:click={toggleModal} on:keydown={toggleModal} active={sticky}/>
+    </cyan-top-app-bar>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+    <p>... scroll ...</p>
+  </article>
+</main>
