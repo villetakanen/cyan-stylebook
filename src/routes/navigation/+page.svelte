162cc105aaaa55106ef172e910f0d3c3532b6732
Ville Takanen <ville.takanen@futurice.com>
2022-11-25 08:52:47 +0200

    fix: sidebar icons

--- src/routes/navigation/+page.svelte
+++ src/routes/navigation/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-	import AppBar from "$lib/AppBar.svelte";
-	import MenuButton from "$lib/MenuButton.svelte";
+import AppBar from "$lib/AppBar.svelte"
+import MenuButton from "$lib/MenuButton.svelte"
 </script>
 
 <AppBar />
@@ -8,7 +8,12 @@
 <main class="bookLayout">
   <article class="Column medium">
 	<h2>Navigation Bar/Tray Buttons</h2>
-    <cyan-navigation-button noun="font" label="Typography" />
+  <p>Navigation Bar and Tray use material -inspired buttons. These buttons should be enclosed in
+    <cyan-code>{'<a></a>'}</cyan-code> tags</p>
+    <cyan-toolbar>
+      <cyan-navigation-button noun="font" label="Typography" />
+      <cyan-navigation-button noun="mekanismi" />
+    </cyan-toolbar>
   </article>
   <MenuButton />
 </main>
