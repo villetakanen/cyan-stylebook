72bcf34165ed6679d150305253ece481ef0ad028
Ville Takanen <ville.takanen@futurice.com>
2022-12-01 12:29:54 +0200

    fix: fieldset added

---
+++ src/lib/FormSection.svelte
@@ -0,0 +1,30 @@
+<script>
+	import MainClassSelect from "./MainClassSelect.svelte";
+
+</script>
+<article class="Column">
+  <h2>Form Section</h2>
+  <p>We provide a responsive styling class <cyan-code>.fieldset</cyan-code> for form sections. This class allows us
+    to simply smash all sorts of inputs and cyan-input fields to a semi coherent stucture in a form.</p>
+
+  <cyan-card>
+  <h3>Example</h3>
+  <section class="fieldset">
+    <cyan-textfield label="First Name" placeholder="First Name" />
+    <cyan-textfield label="Last Name" placeholder="Last Name" />
+    <cyan-textfield label="Email" placeholder="Email" />
+    <MainClassSelect />
+    <cyan-toolbar>
+      <cyan-spacer />
+      <cyan-button text label="Cancel"></cyan-button>
+      <cyan-button label="Submit"></cyan-button>
+    </cyan-toolbar>
+  </section>
+  <section class="fieldset">
+    <cyan-toolbar>
+      <cyan-textfield label="First Name" placeholder="First Name" />
+      <cyan-textfield label="Last Name" placeholder="Last Name" style="flex-grow:1" />
+    </cyan-toolbar>
+  </section>
+</cyan-card>
+</article>
--- src/routes/inputs/+page.svelte
+++ src/routes/inputs/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import AppBar from "$lib/AppBar.svelte";
+import FormSection from "$lib/FormSection.svelte";
 import SelectStory from "$lib/SelectStory.svelte"
 
 </script>
 
 <main class="bookLayout">
+  <FormSection />
   <SelectStory />
 </main>
