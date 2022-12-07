d77f70291749976775e2852ecbc8e741c90721a2
Ville Takanen <ville.takanen@futurice.com>
2022-12-07 14:38:51 +0200

    chore: removed extra build artifacts

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,17 +7,9 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
-	// default options are shown
-    adapter: adapter({
-		// if true, will create a Netlify Edge Function rather
-		// than using standard Node-based functions
-		edge: false,
-  
-		// if true, will split your app into multiple functions
-		// instead of creating a single one for the entire app.
-		// if `edge` is true, this option cannot be used
-		split: false
-	  })
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
