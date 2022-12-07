d4d39c45e529568ec076d2791fe10c5220ff6d6e
Ville Takanen <ville.takanen@futurice.com>
2022-12-07 14:28:53 +0200

    chore: netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,9 +7,17 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+	// default options are shown
+    adapter: adapter({
+		// if true, will create a Netlify Edge Function rather
+		// than using standard Node-based functions
+		edge: false,
+  
+		// if true, will split your app into multiple functions
+		// instead of creating a single one for the entire app.
+		// if `edge` is true, this option cannot be used
+		split: false
+	  })
 };
 
 export default config;
