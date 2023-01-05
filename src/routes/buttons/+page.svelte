e86ef8f9a07cda1f111ea6332a8e5c332106a982
Ville Takanen <ville.takanen@futurice.com>
2023-01-05 13:19:03 +0200

    fix: disabled buttons demo

--- src/routes/buttons/+page.svelte
+++ src/routes/buttons/+page.svelte
@@ -1,3 +1,13 @@
+<script lang="ts">
+	import type { CyanToggle } from "src/cyan-elements/src";
+
+
+let disabled = false
+function setDisabled (e: Event) {
+  disabled = (e.target as CyanToggle).checked
+}
+</script>
+
 <main class="bookLayout">
   <article class="Column small">
     <cyan-card elevation="1" style="text-align: center">
@@ -12,6 +22,15 @@
       <cyan-fab noun="add" secondary>Secondary Fabs?</cyan-fab>
       <cyan-fab noun="delete" small>Small Fabs O_o</cyan-fab>
       <cyan-fab noun="spacer" small secondary>Secondary, with spacer</cyan-fab>
+    </section>
+  </article>
+  <article class="small Column">
+    <h3>Buttons</h3>
+    <section class="fieldset">
+      <cyan-toggle checked={disabled} on:change={setDisabled} label="Disables"/>
+      <cyan-button disabled={disabled} label="Button"></cyan-button>
+      <cyan-button disabled={disabled} secondary label="Secondary"></cyan-button>
+      <cyan-button disabled={disabled} text label="Text"></cyan-button>
     </section>
   </article>
 </main>
