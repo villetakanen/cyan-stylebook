60c6e72c76466ceabe86d68657d532f09eab9856
Ville Takanen <ville.takanen@futurice.com>
2023-01-06 21:11:07 +0200

    fix: stylebook page for plain css buttons

--- src/routes/buttons/+page.svelte
+++ src/routes/buttons/+page.svelte
@@ -26,11 +26,33 @@ function setDisabled (e: Event) {
   </article>
   <article class="small Column">
     <h3>Buttons</h3>
-    <section class="fieldset">
-      <cyan-toggle checked={disabled} on:change={setDisabled} label="Disables"/>
-      <cyan-button disabled={disabled} label="Button"></cyan-button>
-      <cyan-button disabled={disabled} secondary label="Secondary"></cyan-button>
-      <cyan-button disabled={disabled} text label="Text"></cyan-button>
-    </section>
+    <h4>CSS Button Styles</h4>
+    <p class="TypeBody2">Shadow DOM and ability to capsulate functionality - means 
+      we need to re-implement lots of functionality offered by browsers for the standard
+      html button element. Thus: providing a styling for the standard button is simpler
+      to implement - and far more functionable, than using a Web Component to style a 
+      _standard_ button.</p>
+    <p class="TypeBody2">All of these styles except a a parent (likely <cyan-code>{'<body>'}</cyan-code>) with
+      the class <cyan-code>{'.cyan'}</cyan-code></p>
+      <section class="fieldset">
+        <cyan-toggle checked={disabled} on:change={setDisabled} label="Disabled"/>
+        <button disabled={disabled}>Button</button>
+        <button disabled={disabled} class="secondary">Secondary</button>
+        <button disabled={disabled} class="text">Text</button>
+        <button disabled={disabled}><cyan-icon noun="d20"/></button>
+        <button disabled={disabled} class="secondary"><cyan-icon noun="d20"/></button>
+        <button disabled={disabled} class="text"><cyan-icon noun="d20"/></button>
+        <button disabled={disabled}><cyan-icon noun="d20"/> Button + Icon</button>
+        <button disabled={disabled} class="secondary"><cyan-icon noun="d20"/> Secondary + Icon</button>
+        <button disabled={disabled} class="text"><cyan-icon noun="d20"/> Text + Icon</button>
+      </section>
+      <h4>Cyan-button</h4>
+      <p class="TypeCaption"><i>the cyan-button</i> is a dev-time legacy element. By 1.0.0, this should be replaced with a regular button</p>
+      <section class="fieldset">
+        <cyan-toggle checked={disabled} on:change={setDisabled} label="Disabled"/>
+        <cyan-button disabled={disabled} label="Button"></cyan-button>
+        <cyan-button disabled={disabled} secondary label="Secondary"></cyan-button>
+        <cyan-button disabled={disabled} text label="Text"></cyan-button>
+      </section>
   </article>
 </main>
