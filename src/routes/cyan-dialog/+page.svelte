6aff7d682241d5582c80ea4b376d97fc5930893e
Ville Takanen <ville.takanen@futurice.com>
2023-01-17 13:09:20 +0200

    fix: books icons

--- src/routes/cyan-dialog/+page.svelte
+++ src/routes/cyan-dialog/+page.svelte
@@ -1,31 +1,32 @@
 <script lang="ts">
-let dialog = false;
+	import type { CyanDialog } from "src/cyan-elements/src";
 
-const onkey = (e:KeyboardEvent) => {
-  if (e.key === 'Enter') {
-    dialog = true;
-  }
-};
+
+let rval = ''
 const onclose = (e:Event) => {
-  dialog = false;
-};
+  const rval = (e.target as CyanDialog).returnValue;
+}
+
+function openDialog() {
+    const dialog = document.querySelector('cn-dialog') as CyanDialog;
+    dialog.showModal()
+}
+
 </script>
 <main class="bookLayout">
   <article class="Column small">
     <cyan-button
       noun="add"
       label="Open Dialog"
-      on:keydown={onkey}
-      on:click={() => {
-        dialog = true;
-      }} />
+      on:keydown={openDialog}
+      on:click={openDialog} />
   </article>
-  <cyan-dialog open={dialog} title="An Example Dialog, with very long and arduous title" on:close={onclose}>
+  <cn-dialog title="An Example Dialog, with very long and arduous title" on:close={onclose}>
     <cn-tabs tabs={[['test', 'Test'], ['test2', 'Test 2'], ['test3', 'Test 3 - Long Tab Name']]} active="test" />
     <p>Here is a dialog</p>
     <p>It has a title and a close button</p>
     <p>It can be closed by clicking the close button or by pressing the escape key</p>
     <cyan-button noun="close" on:click={onclose} on:keydown={onclose} />
     <cyan-lightmode-toggle />
-  </cyan-dialog>
+  </cn-dialog>
 </main>
